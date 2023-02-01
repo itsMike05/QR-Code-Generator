24fbd5d17a0a3cf10a8ae4cb6db5f85be8286544
Michał Pelc <michal.pelc@live.com>
2023-02-01 20:19:06 +0100

    Created basic UI

--- qr_gen.py
+++ qr_gen.py
@@ -4,13 +4,9 @@ from tkinter import *
 from tkinter import filedialog
 
 
-# GUI
-screen = Tk()
-title = screen.title("QR Code Generator")
-canvas = Canvas(screen, width = 800, height = 600)
-canvas.pack()
-
-
+def selectPath():
+    path = filedialog.askdirectory()
+    
 
 
 # Defining a function to generate the QR code
@@ -25,9 +21,30 @@ def generate_qrcode (url_to_convert):
 
     qr.add_data(url_to_convert)
     qr.make(fit = True)
-    img = qr.make_image(fill_color = 'black', back_color = 'white')
-    img.save('qr_img.png')
+    img = qr.make_image(fill_color = "black", back_color = "white")
+    img.save("qr_img.png")
 
+
+# GUI
+screen = Tk()
+title = screen.title("QR Code Generator")
+canvas = Canvas(screen, width = 600, height = 600)
+canvas.pack()
+
+# Target link field 
+
+link_field = Entry(screen, width = 50)
+link_label = Label(screen, text = "Enter a link to convert: ", font = "Arial, 15")
+canvas.create_window(300, 240, window = link_label)
+canvas.create_window(300, 280, window = link_field)
+
+# Selecting the image path with a select_button
+
+path_label = Label(screen, text = "Choose a path for your image", font = "Arial, 15")
+select_path_button = Button(screen, text = "Open File Explorer", command = selectPath)
+
+canvas.create_window(300, 360, window = path_label)
+canvas.create_window(300, 400, window = select_path_button)
 
 # Rendering GUI
 screen.mainloop()
