4c761f2c954726374de2c7516ca2195ddf6b3ceb
Michał Pelc <michal.pelc@live.com>
2023-02-01 18:47:42 +0100

    Defined a function to generate the QR code, working on the GUI

--- qr_gen.py
+++ qr_gen.py
@@ -1,12 +1,22 @@
-
 # Importing libraries
-
-
+import qrcode 
 
 # Defining a function to generate the QR code
 
+def generate_qrcode (url_to_convert):
 
+    qr = qrcode.QRCode(
+        version = 1, 
+        error_correction = qrcode.constants.ERROR_CORRERT_M, 
+        box_size = 10, 
+        border = 4
+    )
 
-
+    qr.add_data(url_to_convert)
+    qr.make(fit = True)
+    img = qr.make_image(fill_color = 'black', back_color = 'white')
+    img.save('qr_img.png')
 
 # Calling the function
+
+generate_qrcode('www.google.com')
